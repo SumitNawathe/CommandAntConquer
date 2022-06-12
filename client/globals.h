11080a6a943362d07d786516b05512b29fea23ad
Larry Du <ldu0893@gmail.com>
2022-06-12 14:38:53 -0400

    relative pathing for directories

--- client/globals.h
+++ client/globals.h
@@ -3,6 +3,6 @@
 static int SCREEN_WIDTH = 800;
 static int SCREEN_HEIGHT = 600;
 constexpr static bool DEBUG = true;
-constexpr static const char IMAGES_DIR[] = "C:/Users/Sumit Nawathe/source/repos/CommandAntConquer/CommandAntConquer/CommandAntConquer/client/images/";
-constexpr static const char SHADERS_DIR[] = "C:/Users/Sumit Nawathe/source/repos/CommandAntConquer/CommandAntConquer/CommandAntConquer/client/shaders/";
+constexpr static const char IMAGES_DIR[] = "../client/images/";
+constexpr static const char SHADERS_DIR[] = "../client/shaders/";
 
