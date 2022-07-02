20ff3b65c052d1377168f613406bb406311f974c
SumitNawathe <50125892+SumitNawathe@users.noreply.github.com>
2022-07-02 12:25:55 -0400

    minor edits to utils

--- client/utils.h
+++ client/utils.h
@@ -7,21 +7,21 @@ constexpr static glm::vec3 iHat(1.0f, 0.0f, 0.0f);
 constexpr static glm::vec3 jHat(0.0f, 1.0f, 0.0f);
 constexpr static glm::vec3 kHat(0.0f, 0.0f, 1.0f);
 
-glm::vec3 vec2to3(glm::vec2 v) noexcept {
+inline glm::vec3 vec2to3(glm::vec2 v) noexcept {
 	return glm::vec3(v.x, v.y, 0.0f);
 }
 
-glm::vec2 vec3to2(glm::vec2 v) noexcept {
+inline glm::vec2 vec3to2(glm::vec2 v) noexcept {
 	return glm::vec2(v.x, v.y);
 }
 
-long long int currTimeLL(void) {
+inline long long int currTimeLL(void) {
 	return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
 }
 
-long long int genRandLL(void) {
+inline long long int genRandLL(void) {
 	std::random_device rd;
 	std::mt19937_64 gen(rd());
-	std::uniform_int_distribution<long long> distribution;
+	std::uniform_int_distribution<long long int> distribution;
 	return distribution(gen);
 }
